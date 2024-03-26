74667eebf14e637aab448e7677cf28a2d51c20fa
Jamo Luhrsen <jluhrsen@gmail.com>
2024-03-26 14:59:46 -0700

    more descriptive suite names and start pushing to testspace
    Signed-off-by: Jamo Luhrsen <jluhrsen@gmail.com>

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -94,5 +94,7 @@ func TestE2E(t *testing.T) {
 		}
 	}
 	gomega.RegisterFailHandler(framework.Fail)
-	ginkgo.RunSpecsWithDefaultAndCustomReporters(t, "E2e Suite", r)
+	defaultReporterConfigType := config.DeprecatedGinkgoConfigType{}
+	ginkgo.RunSpecsWithDefaultAndCustomReporters(t, fmt.Sprintf("E2e Suite %v%02d", framework.TestContext.ReportPrefix, defaultReporterConfigType.ParallelNode), r)
+
 }
